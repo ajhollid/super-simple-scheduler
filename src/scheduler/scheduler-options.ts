b539bc44a0470d11abb2aae1ac85b2c0b3c9f010
Alex Holliday <ajhollid@gmail.com>
2025-08-07 11:32:30 -0700

    update readme, tests

--- src/scheduler/scheduler-options.ts
+++ src/scheduler/scheduler-options.ts
@@ -1,7 +1,5 @@
 export type SchedulerOptions = {
-  storeType: "inMemory" | "mongo" | "redis";
   logLevel?: "none" | "info" | "debug" | "warn" | "error";
   dev?: boolean;
   processEvery?: number;
-  dbUri?: string;
 };
